6a173d1ac03faba96ac75964afa40964f7d61f06
Sagar Shah <sagarshahj@gmail.com>
2018-10-20 18:35:24 +0530

    side by side view

---
+++ README.md
@@ -0,0 +1,14 @@
+# Marine Tracker
+
+### Description
+This is a simple app which has data of ships with their positions. It is used to track the ships on the map at the given time.
+
+### Requirements
+
+1. Node 8 or later
+2. Node package manager
+
+### Installation
+1. Run `npm install`
+2. After the packages are installed, run `npm start`
+3. Once the server starts, type the following url in the browser `localhost:3000`
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('mainapp', ['ngRoute'])
+angular.module('mainapp', ['ngRoute','angularUtils.directives.dirPagination', 'ngMap'])
 
 .config(['$routeProvider',function($routeProvider) {
     $routeProvider
@@ -9,6 +9,41 @@ angular.module('mainapp', ['ngRoute'])
     .otherwise({redirectTo: '/'});
 }])
 
-.controller('mainCtrl',function() {
+.controller('mainCtrl',function($http, $scope, NgMap, $filter) {
+  $scope.search = {};
+  $scope.ships = []; //declare an empty array
+  $scope.data = {};
+  $scope.itemsPerPage = 15; // can be dynamic wrt server
+  $http.get("javascripts/data1.json").then(function(response){ 
+      $scope.ships = response.data;  //ajax request to fetch data into $scope.data
+      // convert timestamp to dateobj
+      for (var i = 0; i < response.data.length; i++) {
+        $scope.ships[i]['date'] = new Date(response.data[i]['date']).toLocaleString().split(',')[0];
+      }
+      $scope.data.total_count = 36;  // total data counts from server. currently hardcoded
+  });
+  
+  $scope.sort = function(keyname){
+      $scope.sortBy = keyname;   //set the sortBy to the param passed
+      $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+  }
 
+  $scope.view_details = function(ship){
+    $scope.selected_ship = ship;
+    $('#shipDetails').modal('show');
+  }
+
+  $scope.filterDate = function() {
+    $scope.search.date = new Date($scope.search_date).toLocaleString().split(',')[0];;
+    console.log($scope.search.date);
+  }
+
+  $scope.getData = function(page_number){
+    $http.get("javascripts/data" + page_number + '.json').then(function(response){ 
+      $scope.ships = response.data;  //ajax request to fetch data into $scope.data
+      for (var i = 0; i < response.data.length; i++) {
+        $scope.ships[i]['date'] = new Date(response.data[i]['date']);
+      }
+    });
+  }
 });
