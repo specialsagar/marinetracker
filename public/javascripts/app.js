1e31a65515845cebc208cda4b9da6fbff1e55131
Sagar Shah <sagarshahj@gmail.com>
2018-10-27 03:08:32 +0530

    admin dashboard with analytics

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,28 @@
-angular.module('mainapp', ['ngRoute','angularUtils.directives.dirPagination', 'ngMap'])
+angular.module('mainapp', ['ngRoute','angularUtils.directives.dirPagination', 'ngMap', 'chart.js'])
 
 .config(['$routeProvider',function($routeProvider) {
     $routeProvider
-    .when("/", {
-      templateUrl: "templates/first.html",
+    .when("/dashboard", {
+      templateUrl: "templates/dashboard.html",
       controller: "mainCtrl"
     })
-    .otherwise({redirectTo: '/'});
-}])
+    .when("/analytics", {
+      templateUrl: "templates/analytics.html",
+      controller: "analyticsCtrl"
+    })
+    .otherwise({redirectTo: '/dashboard'});
+}]).run(function($rootScope, $location) {
+  $rootScope.$on('$routeChangeStart', function(next, current) {
+    if($location.path() == '/dashboard') {
+      $rootScope.is_dash = true;
+      $rootScope.is_analytics = false;
+    }
+    if($location.path() == '/analytics') {
+      $rootScope.is_dash = false;
+      $rootScope.is_analytics = true;
+    }
+  });
+})
 
 .controller('mainCtrl',function($http, $scope, NgMap, $filter) {
   $scope.search = {};
@@ -15,7 +30,7 @@ angular.module('mainapp', ['ngRoute','angularUtils.directives.dirPagination', 'n
   $scope.ships = []; //declare an empty array
   $scope.data = {};
   $scope.itemsPerPage = 15; // can be dynamic wrt server
-  $http.get("javascripts/data1.json").then(function(response){ 
+  $http.get("javascripts/data/data1.json").then(function(response){ 
       $scope.ships = response.data;  //ajax request to fetch data into $scope.data
       // convert timestamp to dateobj
       for (var i = 0; i < response.data.length; i++) {
@@ -40,11 +55,104 @@ angular.module('mainapp', ['ngRoute','angularUtils.directives.dirPagination', 'n
   }
 
   $scope.getData = function(page_number){
-    $http.get("javascripts/data" + page_number + '.json').then(function(response){ 
+    $http.get("javascripts/data/data" + page_number + '.json').then(function(response){ 
       $scope.ships = response.data;  //ajax request to fetch data into $scope.data
       for (var i = 0; i < response.data.length; i++) {
         $scope.ships[i]['date'] = new Date(response.data[i]['date']);
       }
     });
   }
+})
+
+.controller('analyticsCtrl',function($http, $scope, $filter, ) {
+  $scope.scope1 = 'country';
+  $scope.scope2 = 'origin';
+  $scope.scope3 = 'China';
+
+  $http.get("javascripts/data/alldata.json").then(function(response){ 
+    $scope.ships = response.data;  //ajax request to fetch data into $scope.data
+    $scope.typeAnalytics();
+    $scope.portAnalytics();
+    $scope.containerAnalytics();
+  });
+
+  $scope.typeAnalytics = function(){
+    $scope.country_category_data = [];
+    if ($scope.scope1 == 'country') {
+      $scope.country_category_labels = ['China', 'India', 'Singapore', 'UAE'];
+      for (var i = 0; i < $scope.country_category_labels.length; i++) {
+        $scope.country_category_data.push($filter('filter')($scope.ships, {country: $scope.country_category_labels[i]}).length);
+      }
+    } else {
+      $scope.country_category_labels = ['Chemicals', 'Consumer Goods', 'Food Products', 'Metals'];
+      var contentArr = [];
+      for (var i = 0; i < $scope.ships.length; i++) {
+        contentArr.push($scope.ships[i]['content']);
+      }
+      contentArr = [].concat.apply([], contentArr);
+      console.log(contentArr);
+      for (var i = 0; i < $scope.country_category_labels.length; i++) {
+        $scope.country_category_data.push($filter('filter')(contentArr, {category: $scope.country_category_labels[i]}).length);
+      }
+    }
+  }
+
+  $scope.portAnalytics = function(){
+    $scope.origin_dest_labels = ['Dubai', 'Mumbai', 'Kochi', 'Chennai', 'Karachi'];
+    $scope.origin_dest_data = [];
+    if ($scope.scope2 == 'origin') {
+      for (var i = 0; i < $scope.origin_dest_labels.length; i++) {
+        $scope.origin_dest_data.push($filter('filter')($scope.ships, {origin: $scope.origin_dest_labels[i]}).length);
+      }
+    } else {
+      for (var i = 0; i < $scope.origin_dest_labels.length; i++) {
+        $scope.origin_dest_data.push($filter('filter')($scope.ships, {destination: $scope.origin_dest_labels[i]}).length);
+      }
+    }
+  }
+
+  $scope.containerAnalytics = function(){
+    $scope.series = [];
+    if ($scope.scope3 == '') {
+      $scope.series = ['China', 'India', 'Singapore', 'UAE'];
+    } else {
+      $scope.series = [$scope.scope3];
+      $scope.bardata = [[]];
+      $scope.barlabels = [];
+      var filteredData =  $filter('filter')($scope.ships, {country: $scope.scope3});
+      for (var i = 0; i < filteredData.length; i++) {
+        $scope.bardata[0].push(filteredData[i]['content'].length);
+        $scope.barlabels.push(filteredData[i]['name']);
+      }
+    }
+  }
+
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1', steppedLine: true, borderColor: $scope.colors, backgroundColor: $scope.colors }];
+  $scope.options = {
+    scales: {
+      responsive: true,
+      xAxes: [{
+        id: 'x-axis-1',
+        display: true,
+        scaleLabel: {
+          display: true
+        }
+      }],
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Number of containers'
+          },
+          ticks:{
+              min: 0,
+              suggestedMax: 10
+          },
+          position: 'left'
+        }
+      ]
+    }
+  };
 });
